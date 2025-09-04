eea148ac6eea19473218ac970ac158b5299c92c6
Threcia-01 <deepchaudhary29924@gmail.com>
2025-09-04 20:28:03 +0530

    Remove node_modules from repo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import dotenv from "dotenv"
 import {app} from "./app.js";
+import connectDB from "./db/index.js";
 
 dotenv.config({
     path:"./.env"
 })
 
-const PORT = 7000
+const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
 });
+
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDb connection error",err)
+})
