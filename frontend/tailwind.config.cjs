64bf2ed081867d8215160f802c066c4fecbd11f6
Threcia-01 <155178412+Threcia-01@users.noreply.github.com>
2025-10-16 12:10:27 +0530

    Remove commented line for line-clamp plugin
    Removed commented line for line-clamp plugin in Tailwind config.

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,10 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [
-    // require('@tailwindcss/line-clamp') // enable if you installed the plugin
   ],
 };
